c5059ae4f05441efb4e20cf4359d6b314c1332f5
Josmar Barrios <admin@posperpanama.com>
2026-05-14 11:13:09 -0500

    footerrer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Footer from "@/components/Footer"; // Importamos el footer
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,10 +28,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}>
+        {/* El contenido ocupa el espacio y empuja el footer hacia abajo */}
+        <div className="flex-grow">
+          {children}
+        </div>
+        
+        {/* El Footer al final de la página */}
+        <Footer />
       </body>
     </html>
   );
